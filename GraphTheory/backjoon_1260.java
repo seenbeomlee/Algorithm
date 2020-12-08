89552ac61e721c6a74c88fac121adf569037a81e
seenblee1 <sblee@autocrypt.io>
2020-12-08 09:19:42 +0900

    #java: backjoon: GraphTheory > DFS, BFS

--- GraphTheory/backjoon_1260.java
+++ GraphTheory/backjoon_1260.java
@@ -8,27 +8,33 @@ class Main {
     int M = scn.nextInt(); 
     int V = scn.nextInt();
 
+    int[] checkList = new int[N];
+
     int[][] graph = new int[N][N];
+    int[][] graphDFS = new int[N][N];
+    int[][] graphBFS = new int[N][N];
+
     for(int i = 0; i < M; i++) {
       int from = scn.nextInt(); 
       int to = scn.nextInt();
       graph[from - 1][to - 1] = 1;
       graph[to - 1][from - 1] = 1;
+
+      graphDFS[from - 1][to - 1] = 1;
+      graphDFS[to - 1][from - 1] = 1;
+
+      graphBFS[from - 1][to - 1] = 1;
+      graphBFS[to - 1][from - 1] = 1;
     }
 
-    int[][] graphDFS = new int[N][N];
-    int[][] graphBFS = new int[N][N];
-
-    graphDFS = graph.clone();
-    graphBFS = graph.clone();
-
-    DFS(graphDFS, N, V);
+    DFS(graphDFS, checkList, N, V);
     System.out.println();
-    BFS(graphBFS, N, V);
+    Arrays.fill(checkList, 0); // init to 0
+    BFS(graphBFS, checkList, N, V);
 
   }
 
-  public static void DFS(int[][] graph, int N, int V) {
+  public static void DFS(int[][] graph, int[] checkList, int N, int V) {
     //stack
     //https://coding-factory.tistory.com/601?category=758267
     Stack<Integer> stack = new Stack<>();
@@ -37,20 +43,21 @@ class Main {
 
     while(!stack.isEmpty()) {
       now = stack.pop();
-      System.out.print(now + 1 + " ");
+      if(checkList[now] == 0) {
+        System.out.print(now + 1 + " ");
+        checkList[now] = 1;
+      }
       for(int i = N - 1; i >= 0; i--) {
         if(graph[now][i] == 1) {
           stack.push(i);
           graph[now][i] = 0;
-          for(int j = 0; j < N; j++)
-            graph[i][j] = 0;
         }
       }
     }
     //deque
   }
 
-  public static void BFS(int[][] graph, int N, int V) {
+  public static void BFS(int[][] graph, int[] checkList, int N, int V) {
     //queue
     //https://coding-factory.tistory.com/602
     Queue<Integer> queue = new LinkedList<>();
@@ -59,13 +66,14 @@ class Main {
 
     while(!queue.isEmpty()) {
       now = queue.remove();
-      System.out.print(now + 1 + " ");
+      if(checkList[now] == 0) {
+        System.out.print(now + 1 + " ");
+        checkList[now] = 1;
+      }
       for(int i = 0; i < N; i++) {
         if(graph[now][i] == 1) {
           queue.add(i);
           graph[now][i] = 0;
-          for(int j = 0; j < N; j++)
-            graph[i][j] = 0;
         }
       }
     }
