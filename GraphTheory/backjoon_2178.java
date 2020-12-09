a3217a38981492d86e4681fbb21817374351a0af
seenblee1 <sblee@autocrypt.io>
2020-12-09 10:18:30 +0900

    #java: backjoon: GraphTheory > 2178 FAILED

--- GraphTheory/backjoon_2178.java
+++ GraphTheory/backjoon_2178.java
@@ -63,6 +63,23 @@ import java.util.*;
 //   }
 // }
 
+import java.util.*;
+
+class node {
+  public int x, y;
+  public int is;
+  public int num;
+  public boolean visited;
+
+  node(int j, int i, int num, int num2, boolean bool) {
+    this.x = j;
+    this.y = i;
+    this.is = num;
+    this.num = num2;
+    this.visited = bool;
+  }
+}
+
 class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -72,8 +89,8 @@ class Main {
         String inputLine;
         String trash;
 
-        int[][] graph = new int[N][M];
-        int[][] dp = new int[N][M];
+        node[][] graph = new node[N][M];
+        node[][] sec_graph = new node[N][M];
 
         trash = scn.nextLine();
 
@@ -81,12 +98,58 @@ class Main {
             inputLine = scn.nextLine();
             for(int j = 0; j < M; j++) {
                 // convert string 11011 to int 1 1 0 1 1
-                int temp = (int)inputLine.charAt(j);
-                if(temp == 48) temp = 0;
-                else temp = 1;
+                int num = (int)inputLine.charAt(j);
+                if(num == 48) num = 0;
+                else num = 1;
+                node temp = new node(j, i, num, 9999, false);
                 graph[i][j] = temp; 
-                if(graph[i][j] == 0) dp[i][j] = 9999;
             }
         }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+              sec_graph[i][j] = graph[i][j];
+            }
+        }
+
+        int res = 9999;
+        if(graph[1][0].is == 1) res = Math.min(res, DFS(graph, graph[1][0], N, M));
+        if(graph[0][1].is == 1) res = Math.min(res, DFS(sec_graph, sec_graph[0][1], N, M));
+
+        System.out.print(res + 2);
+    }
+
+    public static int DFS(node[][] graph, node first, int N, int M) {
+      Deque<node> deque = new ArrayDeque<node>();
+      node now;
+      first.num = 0;
+      first.visited = true;
+      deque.add(first);
+      while(!deque.isEmpty()) {
+        now = deque.poll();
+        //now.visited = true;
+        //System.out.println("now : " + now.x + now.y + now.num);
+
+        if(now.y > 0 && !graph[now.y-1][now.x].visited && graph[now.y-1][now.x].is == 1) {
+          //System.out.println("A : " + now.x + now.y + now.num);
+          graph[now.y-1][now.x].num = Math.min(graph[now.y-1][now.x].num, now.num + 1);
+          graph[now.y-1][now.x].visited = true;
+          deque.add(graph[now.y-1][now.x]);
+        }
+        if(now.x < M-1 && !graph[now.y][now.x+1].visited && graph[now.y][now.x+1].is == 1) {
+          //System.out.println("B : " + now.x + now.y + now.num);
+          graph[now.y][now.x+1].num = Math.min(graph[now.y][now.x+1].num, now.num + 1);
+          graph[now.y][now.x+1].visited = true;
+          deque.add(graph[now.y][now.x+1]);
+        }
+        if(now.y < N-1 && !graph[now.y+1][now.x].visited && graph[now.y+1][now.x].is == 1) {
+          //System.out.println("C : " + now.x + now.y + now.num);
+          graph[now.y+1][now.x].num = Math.min(graph[now.y+1][now.x].num, now.num + 1);
+          graph[now.y+1][now.x].visited = true;
+          deque.add(graph[now.y+1][now.x]);
+        }
+      }
+
+      return graph[N-1][M-1].num;
     }
 }
