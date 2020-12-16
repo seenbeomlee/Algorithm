1bd86f537903daa8a6f09462fd143b0bfd9c30d9
seenblee1 <sblee@autocrypt.io>
2020-12-16 11:57:15 +0900

    #cpp: backjoon: sorting > only TIMELIMIT in backjoon becauseof cin & cout <=> scanf & printf

---
+++ Sorting/backjoon_1181.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <algorithm> // sort
+#include <cmath> // floor
+
+using namespace std;
+
+int N, M;
+
+bool compare(string& A, string& B) {
+  if(A.length() < B.length()) return true;
+  else if(A.length() > B.length()) return false;
+  else {
+    return A < B;
+  }
+}
+
+int main() {
+  scanf("%d", &N);
+
+  vector<string> s_list;
+
+  //char* temp;
+  string s_new;
+  for(int i = 0; i < N; i++) {
+    cin >> s_new;
+
+    bool is = false;
+    for(int j = 0; j < s_list.size(); j++) {
+      if(s_list[j] == s_new) is = true;
+    }
+    
+    if(!is) s_list.push_back(s_new);
+  }
+
+  sort(s_list.begin(), s_list.end(), compare);
+
+  for(int i = 0; i < s_list.size(); i++) {
+    printf("%s\n", s_list[i].c_str());
+  }
+}
