a80a78c42db7307d29c2ca5d729b0310c225e2a2
seenblee <mail@seenblee.com>
2020-12-18 15:09:56 +0900

    #cpp: backjoon: Divide&Conquer > 6549 ing

---
+++ Divide&Conquer/backjoon_6549.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+#include <algorithm> // sort
+#include <cmath> // floor
+
+using namespace std;
+
+long long N;
+
+int main() {
+  while(1) {
+    scanf("%lld", &N); // 1 <= N <= 100,000
+    if(N == 0) break;
+
+    long long* input = new long long[N]; // 1 <= N <= 10 * 10^8
+    for(int i = 0; i < N; i++) scanf("%lld", &input[i]);
+
+
+  }
+}
