c8da23c87840d25c52bbb656ce980b5140e6cbdb
seenblee <mail@seenblee.com>
2020-12-23 11:40:58 +0900

    #cpp: backjoon: GraphTheory > 1753 TIMELIMIT

--- GraphTheory/backjoon_1753.cpp
+++ GraphTheory/backjoon_1753.cpp
@@ -1,3 +1,6 @@
+// 다익스트라(dijkstra)
+// https://www.crocus.co.kr/532
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,45 +10,71 @@
 #include <cmath>     // floor
 using namespace std;
 
+class Edge {
+public:
+	int to, weight;
+
+	Edge() { ; };
+	Edge(int a, int b) {
+		to = a;
+		weight = b;
+	}
+};
+
 int main() {
 	int V, E;
 	int K;
 	int u, v, w;
 
-	cin >> V >> E;
-	cin >> K;
+	//cin >> V >> E;
+	//cin >> K;
+	scanf("%d %d", &V, &E);
+	scanf("%d", &K);
 
-	vector<int>* edge_list = new vector<int>[V+1];
+	K--;
 
-	int** board = new int* [V+1];
-	for (int i = 0; i < V+1; i++) board[i] = new int[V+1];
-	for (int i = 0; i < V+1; i++)
-		for (int j = 0; j < V+1; j++)
-			board[i][j] = 11;
+	vector<Edge>* edge_list = new vector<Edge>[V]; // 1 ~ V ==> 0 ~ V-1
 
 	for (int i = 0; i < E; i++) {
-		cin >> u >> v >> w;
+		//cin >> u >> v >> w;
+		scanf("%d %d %d", &u, &v, &w);
 
-		if (board[u][v] == 11) edge_list[u].push_back(v);
-		board[u][v] = min(board[u][v], w);
+		u--;
+		v--;
+
+		Edge temp(v, w);
+		edge_list[u].push_back(temp);
 	}
 
-	int* dist = new int[V+1];
-	bool* visited = new bool[V+1];
-	for (int i = 0; i < V; i++) {
-		dist[i] = INT_MAX;
-		visited[i] = false;
-	}
+	int* dist = new int[V];
+	bool* visited = new bool[V];
+	for (int i = 0; i < V; i++)
+		dist[i] = 999999, visited[i] = false;
+
 	dist[K] = 0;
-	visited[K] = true;
 	int now = K;
-	while (1) {
 
+	for(int i = 0; i < V-1; i++) {
+		visited[now] = true;
 
+		for (Edge edge : edge_list[now]) {
+			if(visited[edge.to] == false) dist[edge.to] = min(dist[edge.to], dist[now] + edge.weight);
+		}
 
-
+		int min = 999999;
 		for (int i = 0; i < V; i++) {
-			if(visited)
+			if (visited[i] == false && min > dist[i]) {
+				min = dist[i];
+				now = i;
+			}
 		}
 	}
+
+	for (int i = 0; i < V; i++) {
+		//if (dist[i] == 999999) cout << "INF" << endl;
+		//else cout << dist[i] << endl;
+		if (dist[i] == 999999) printf("INF\n");
+		else printf("%d\n", dist[i]);
+	}
 }
+
