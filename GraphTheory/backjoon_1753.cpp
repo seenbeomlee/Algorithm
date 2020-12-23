06aeb83dbb94425eb772f8305603d8ec6cb80a0f
seenblee <mail@seenblee.com>
2020-12-23 12:01:12 +0900

    #cpp: backjoon: GraphTheory > Dijkstra

--- GraphTheory/backjoon_1753.cpp
+++ GraphTheory/backjoon_1753.cpp
@@ -1,80 +1,69 @@
-// 다익스트라(dijkstra)
-// https://www.crocus.co.kr/532
+// 다익스트라 Dijkstra
+// https://eine.tistory.com/entry/%EB%B0%B1%EC%A4%80-1753%EB%B2%88-%EB%AC%B8%EC%A0%9C-%EC%B5%9C%EB%8B%A8%EA%B2%BD%EB%A1%9C-%ED%92%80%EC%9D%B4
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <deque>
+// Pair - first를 먼저 비교하고, first가 같은 경우 second의 대소 비교
+// Priority_queue는 기본적으로 MAX_HEAP 이므로, '-'를 넘어서 음수 값을 넣는 형식으로 MIN_HEAP 구현
 
-#include <algorithm> // sort
-#include <cmath>     // floor
-using namespace std;
+#include <bits/stdc++.h> 
 
-class Edge {
-public:
-	int to, weight;
+using namespace std; 
 
-	Edge() { ; };
-	Edge(int a, int b) {
-		to = a;
-		weight = b;
-	}
-};
+typedef long long ll; 
+typedef pair<int, int> pii; 
 
-int main() {
-	int V, E;
-	int K;
-	int u, v, w;
+#define INF 2000000000 
+#define MAXN 20004 
 
-	//cin >> V >> E;
-	//cin >> K;
-	scanf("%d %d", &V, &E);
-	scanf("%d", &K);
+vector<pii> edges[MAXN]; //dest, weight 
+bool visit_check[MAXN]; 
+int dist[MAXN]; 
 
-	K--;
+int main() { 
+  
+  int discovered = 0; 
+  int V, E, K; 
+  
+  scanf("%d%d%d", &V, &E, &K); 
+  
+  for (int i = 0; i <= V; i++) { 
+    dist[i] = INF; // K to nodeV distance init
+  } 
+  
+  for (int i = 0; i < E; i++) { 
+    int u, v, w; scanf("%d%d%d", &u, &v, &w); 
+    edges[u].push_back({ v, w }); 
+  } 
+    
+  priority_queue<pii> pq; // -dist, index 
+  pq.push({ 0, K }); // K has '0' distance to K
 
-	vector<Edge>* edge_list = new vector<Edge>[V]; // 1 ~ V ==> 0 ~ V-1
-
-	for (int i = 0; i < E; i++) {
-		//cin >> u >> v >> w;
-		scanf("%d %d %d", &u, &v, &w);
-
-		u--;
-		v--;
-
-		Edge temp(v, w);
-		edge_list[u].push_back(temp);
-	}
-
-	int* dist = new int[V];
-	bool* visited = new bool[V];
-	for (int i = 0; i < V; i++)
-		dist[i] = 999999, visited[i] = false;
-
-	dist[K] = 0;
-	int now = K;
-
-	for(int i = 0; i < V-1; i++) {
-		visited[now] = true;
-
-		for (Edge edge : edge_list[now]) {
-			if(visited[edge.to] == false) dist[edge.to] = min(dist[edge.to], dist[now] + edge.weight);
-		}
-
-		int min = 999999;
-		for (int i = 0; i < V; i++) {
-			if (visited[i] == false && min > dist[i]) {
-				min = dist[i];
-				now = i;
-			}
-		}
-	}
-
-	for (int i = 0; i < V; i++) {
-		//if (dist[i] == 999999) cout << "INF" << endl;
-		//else cout << dist[i] << endl;
-		if (dist[i] == 999999) printf("INF\n");
-		else printf("%d\n", dist[i]);
-	}
+  while (pq.size()) { // !pq.empty()
+    pii cur = pq.top(); 
+    pq.pop(); 
+    int distance = -cur.first; 
+    int current = cur.second; 
+    
+    if (visit_check[current]) continue; 
+    if (dist[current] == INF) discovered++; 
+    
+    visit_check[current] = true; 
+    dist[current] = distance; 
+    
+    if (discovered == V) break; 
+    
+    for (pii nx : edges[current]) { 
+        int next = nx.first; 
+        int weight = nx.second; 
+        pq.push({ -dist[current] - weight, next });
+        // next가 같은 경우 중복 고려하지 않고 priorit_queue에 다 넣어도
+        // MIN_HEAP에 알아서 weight가 작은 첫번 째 것만 search
+    } 
+  } 
+    
+    for (int i = 1; i <= V; i++) { 
+      if (dist[i] == INF) { puts("INF"); } 
+      else { printf("%d\n", dist[i]); } 
+    } 
+      
+  return 0; 
 }
-
